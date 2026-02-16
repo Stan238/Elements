1a5a71286af62b38786207c331c3d85ea2e8a532
Stan238 <127895826+Stan238@users.noreply.github.com>
2026-02-15 21:01:29 -0500

    created new element button. Change behavior buttons in JS. Also, changed position elements.

--- box.js
+++ box.js
@@ -2,19 +2,36 @@
 const box = document.getElementById("box")
 const buttonStart = document.getElementById('start');
 const buttonClean = document.getElementById('clean');
+const buttonTimer = document.getElementById('timer');
 let count = 10;
 let timer;
 box.textContent ='';
 
+buttonStart.disabled = true;
+buttonTimer.disabled = true;
 
-function startCountdown() {
+setTimeout(()=>{
+    alert(" Choose button'Down' or 'Up'!");
+        buttonStart.disabled = false;
+        buttonTimer.disabled = false;
+}, 10
+);
+
+function buttonsDisable(){
     buttonStart.disabled = true;
     buttonClean.disabled = false;
-    box.textContent = `${count}`;
-
-   timer ??= setInterval(countdown, 1000);
-    
+    buttonTimer.disabled = true;
 }
+
+
+
+
+function startCountdown() {
+   buttonsDisable();
+    box.textContent = `${count}`;
+   timer ??= setInterval(countdown, 1000);
+}
+
 function countdown( ) {
     count--;
     if (count >= 1) {
@@ -24,16 +41,36 @@ function countdown( ) {
         box.textContent = 'END';
     }
 }
+function  timerClock() {
+    count++;
+    box.textContent = `${count}`;
+}
+
+function startTimer() {
+    buttonsDisable();
+    box.textContent = `${count}`;
+    timer ??= setInterval(timerClock, 1000);
+}
 
 
 function clean() {
     clearInterval(timer);
-    timer = null;
-    count = 10;
-    box.textContent = '';
+    if (timer != null) {
+        timer = null;
+    } else {
+        count = 10;
+        buttonClean.disabled = true;
+        box.textContent = `${count}`;
+    }
     buttonStart.disabled = false;
-    buttonClean.disabled = true;
-  
+    buttonTimer.disabled = false;
 }
 
-
+buttonStart.addEventListener("click", () => {
+    box.textContent = '';
+    startCountdown();
+});
+buttonClean.addEventListener("click", clean);
+buttonTimer.addEventListener("click", () => {
+    startTimer();
+});
